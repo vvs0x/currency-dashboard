ec255adf711e31c21c94c8135877129e262fb249
vvs0x <vschwarz@ik.me>
2026-04-02 22:35:17 +0200

    fixed url for fetching monthly data and not yearly

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.fetch_data import FetchData
 import pandas as pd
 
 if __name__ == '__main__':
-    url = 'https://data.snb.ch/api/cube/devkua/data/json/de'
+    url = 'https://data.snb.ch/api/cube/devkum/data/json/de'
     c = FetchData(url=url, retries=5)
     data = c.get_data()
     print(data)

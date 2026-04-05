eb2dd7e11c7e8c1c7066d490f3b419edf51f48b3
vvs0x <vschwarz@ik.me>
2026-04-05 19:05:06 +0200

    set up streamlit

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Currency Dashboard", layout="wide")
 st.title("Currency Dashboard")
 st.write("Hello, world!")
+
+data = [1,2,3,4,5,6,7,8,9]
+st.bar_chart(data, x_label='anything', y_label='anything2')
+
+st.button('hello')
